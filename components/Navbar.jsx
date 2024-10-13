70dfea6f666c7eb39f8ea88459bf0df5e2c75579
7sumona02 <hawkingsumona8@gmail.com>
2024-10-13 16:34:44 +0530

    edits

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,14 +20,12 @@ const Navbar = () => {
             </div>
         </div>
         <div className='ml-2 flex justify-between gap-20'>
-        <div className='searchbar flex items-center border-2 border-neutral-400 rounded-full px-2 pr-14'>
-            <Link href='/' className='text-neutral-600 font-semibold'>https://sumona.crd.co/</Link>
-        </div>
-        <div className=''>
+          <div className='searchbar flex items-center border-2 border-neutral-400 rounded-full px-2 pr-14'>
+              <Link href='/' className='text-neutral-600 font-semibold'>https://sumona.crd.co/</Link>
+          </div>
           <div className='flex justify-center bg-muted p-1 text-neutral-600 rounded-full cursor-pointer border-2 border-neutral-400'>
               <Search size={12} strokeWidth={3} />
           </div>
-        </div>
         </div>
     </div>
   )
