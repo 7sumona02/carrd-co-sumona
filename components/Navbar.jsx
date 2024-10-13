e7049791ab0955599e2d6b1ca869a455bbc5845f
7sumona02 <hawkingsumona8@gmail.com>
2024-10-13 16:40:04 +0530

    edits

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-xs flex'>
+    <div className='text-xs flex w-[32.8vw] relative'>
         <div className='flex gap-1 ml-2'>
             <div className='flex justify-center bg-muted p-1 text-neutral-600 rounded-full cursor-pointer border-2 border-neutral-400'>
                 <ChevronLeft size={12} strokeWidth={3} />
@@ -19,12 +19,12 @@ const Navbar = () => {
                 <RefreshCw size={12} strokeWidth={3} />
             </div>
         </div>
-        <div className='ml-2 flex justify-between gap-20'>
-          <div className='searchbar flex items-center border-2 border-neutral-400 rounded-full px-2 pr-14'>
+        <div className='searchbar flex items-center border-2 border-neutral-400 rounded-full px-2 pr-14 ml-3'>
               <Link href='/' className='text-neutral-600 font-semibold'>https://sumona.crd.co/</Link>
-          </div>
+        </div>
+        <div className='absolute right-2'>
           <div className='flex justify-center bg-muted p-1 text-neutral-600 rounded-full cursor-pointer border-2 border-neutral-400'>
-              <Search size={12} strokeWidth={3} />
+                <Search size={12} strokeWidth={3} />
           </div>
         </div>
     </div>
